323d098282c1cdb75baae83cabfee2a2825ef5a2
Farah Riad Eilouti <fara7.eilouti@gmail.com>
2023-01-16 22:52:03 +0200

    editing form and bg

--- app/components/Form.js
+++ app/components/Form.js
@@ -15,11 +15,13 @@ import {useState} from "react";
 
 return(
   
-    <div class="flex justify-left">
     
-    <form class="bg-white rounded-md shadow-md p-8 w-1/3">
+    <div class="flex justify-center bg-[url(https://images.unsplash.com/photo-1587612049655-c1030366a74a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)] bg-cover bg-center bg-no-repeat "> 
     
-    
+
+    <form class="bg-glass bg-white bg-opacity-70  drop-shadow-lg  rounded-md shadow-md p-10 w-1/3">    
+     
+   
     <label class="block font-medium text-sm mb-2">
     Category:
     <select class="block w-full rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline-blue border border-gray-300">
@@ -90,7 +92,9 @@ return(
     <button class="bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-md mx-auto my-auto flex justify-center" type="submit">Create Class</button>
     
   </form>
+  
   </div> 
+  
 
   
     );

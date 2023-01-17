53af47b149cf80797b1e8750a77f1f607e990f12
Shaden Shanab <shadenshanab2@gmail.com>
2023-01-17 17:13:53 +0000

    add: class page

---
+++ app/class/page.js
@@ -0,0 +1,75 @@
+import React from "react";
+
+export default function Class() {
+
+    return (
+        <>
+           <div class="antialiased max-w-6xl mx-auto my-12 bg-[#E4DCCF] px-8">
+
+
+                <div class="relative block md:flex items-center">
+                    <div class="w-full md:w-1/2 relative z-1 bg-gray-100 rounded shadow-lg overflow-hidden">
+                        <div class="text-xl font-medium text-green-500 uppercase p-8 text-center border-b border-[#cfdddd] tracking-wide">Class Name</div>
+                        <div class="block sm:flex md:block lg:flex items-center justify-center">
+                            <div class="mt-8 sm:m-8 md:m-0 md:mt-8 lg:m-8 text-center">
+                                <div class="inline-flex items-center">
+                                    <span class="text-3xl font-medium">Description</span>
+                                    
+                                </div>
+                                <span class="block text-sm text-gray-600 mt-2">This class is a class that classes, the class teaches class stuff and class things</span>
+                            </div>
+
+                        </div>
+                        <div class="flex justify-center mt-3">
+                            <ul>
+                                
+                                <li class="flex items-center">
+                                    <div class="bg-green-200 rounded-full p-2 fill-current text-green-700">
+
+                                    </div>
+                                    <span class="text-gray-700 text-lg ml-3">Category: Name</span>
+                                </li>
+                                <li class="flex items-center mt-3">
+                                    <div class="bg-green-200 rounded-full p-2 fill-current text-green-700">
+                                    </div>
+                                    <span class="text-gray-700 text-lg ml-3">Available time: 00:00 - 00:00</span>
+                                </li>
+                                <li class="flex items-center mt-3">
+                                    <div class="bg-green-200 rounded-full p-2 fill-current text-green-700">
+                                    </div>
+                                    <span class="text-gray-700 text-lg ml-3">Price: $00.0/hr</span>
+                                </li>
+                            </ul>
+                        </div>
+                        <a class="block flex items-center justify-center bg-[#c7e0e0] hover:bg-[#E4DCCF] p-8 text-md font-semibold text-gray-800 uppercase mt-16" href="#">
+                            {/* If not purchased */}
+                            <span>Purchase</span>
+                            <span class="font-medium text-gray-700 ml-2">➔</span>
+                            {/* if purchased => hide span */}
+                        </a>
+                    </div>
+                    <div class="w-full md:w-1/2 relative z-0 px-8 md:px-0 md:py-16">
+                        <div class="bg-[#576F72] text-white rounded-b md:rounded-b-none md:rounded-r shadow-lg overflow-hidden">
+                            <div class="text-lg font-medium uppercase p-8 text-center border-b border-[#7D9D9C] tracking-wide">Teacher name</div>
+                            <div class="text-center text-sm sm:text-md max-w-sm mx-auto mt-8 text-[#c7e0e0] px-8 lg:px-0">
+                                Teacher Description which describes the teacher abilities and what they do too
+                            </div>
+                            <div class="mt-8 border border-[#7D9D9C] mx-8 lg:mx-16 flex flex-wrap">
+                                <div class="flex items-center justify-center w-1/2 text-center p-4 border-r border-b border-[#7D9D9C]">Email</div>
+                                <div class="flex items-center justify-center w-1/2 text-center p-4 border-b border-[#7D9D9C]">Teacher@teach.com</div>
+                                <div class="flex items-center justify-center w-1/2 text-center p-4 border-r border-[#7D9D9C]">Teacher Category</div>
+                                <div class="flex items-center justify-center w-1/2 text-center p-4">What they teach</div>
+                            </div>
+                            <a class="block flex items-center justify-center bg-[#7D9D9C] hover:bg-[#bbae9a] hover:text-[#576F72] p-8 text-md font-semibold text-[#E4DCCF] uppercase mt-8" href="#">
+                                <span>Teacher Profile</span>
+                                <span class="font-medium text-[#E4DCCF] ml-2 hover:text-[#576F72]">➔</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </>
+    );
+}

3044369f7c28325eebff2b649fa0b870500780bb
Farah Riad Eilouti <fara7.eilouti@gmail.com>
2023-01-25 11:10:56 +0200

    login page farah edits

--- app/loginform/page.js
+++ app/loginform/page.js
@@ -113,61 +113,63 @@ export default function LoginForm() {
   
  
   return (
-    <div class=" h-screen w-screen ">
-      <div class="flex flex-col items-center flex-1 h-full justify-center px-4 sm:px-0 ">
-        <div
-          class="flex rounded-lg shadow-lg w-full sm:w-3/4 lg:w-1/2 
-		bg-white sm:mx-0 bg-opacity-70  drop-shadow-lg  rounded-md shadow-md"
+    <div className="flex justify-center bg-[#576F72] background-size: full;  ">
+      <div className="flex flex-col items-center flex-1 h-full justify-center px-4 sm:px-0 ">
+      <div
+          className="flex rounded-lg shadow-lg w-full sm:w-3/4 lg:w-1/2
+    bg-glass bg-white bg-opacity-70  drop-shadow-lg  rounded-md shadow-md p-10 w-1/3"
           style={{ height: "500px" }}
         >
-          <div class="flex flex-col w-full md:w-1/2 p-4 ">
-            <div class="flex flex-col flex-1 justify-center mb-8">
-              <h1 class="text-4xl text-center font-thin">Welcome Back</h1>
-              <div class="w-full mt-4">
+          <div className="flex flex-col w-full md:w-1/2 p-4 ">
+          <div className="flex flex-col flex-1 justify-center mb-8">
+          <h1 className="text-4xl text-center font-normal">Welcome Back</h1>
+          <div className="w-full mt-4">
+                 
+                 
+                 
                  <form
                   onSubmit={handleSubmit}
-                  class="form-horizontal w-3/4 mx-auto bg-opacity-70  drop-shadow-lg  rounded-md shadow-md"
-                  method="POST"
+                  className="form-horizontal w-3/4 mx-auto bg-opacity-70  drop-shadow-lg  "                  method="POST"
                   action="#"
                 >
-                  <div class="flex flex-col mt-4">
+                    <div class="mb-6">
                     <input
                       type="email"
                       name="email"
+                      class="form-control block w-full px-4 py-2
+                        text-x2 font-normal text-gray-700
+                        bg-white bg-clip-padding border
+                        border-solid border-gray-300
+                        rounded transition ease-in-out m-0
+                        focus:text-gray-700 focus:bg-white
+                        focus:border-blue-600 focus:outline-none"
+                        id="exampleFormControlInput2"
+
                       placeholder="Email"
                       onChange={e => setEmail(e.target.value)}
                       required
                     />
                   </div>
-                  <div class="flex flex-col mt-4">
+
+
+                   <div class="mb-6">
                     <input
                       type="password"
                       name="password"
+                      class="form-control block w-full px-4 py-2
+                        text-x2 font-normal text-gray-700
+                        bg-white bg-clip-padding border
+                        border-solid border-gray-300
+                        rounded transition ease-in-out m-0
+                        focus:text-gray-700 focus:bg-white
+                        focus:border-blue-600 focus:outline-none"
+
                       placeholder="Password"
                       onChange={e => setPassword(e.target.value)}
                       required
                     />
                   </div>
-                  <div class="flex flex-col mt-8">
-                    <button
-                      type="submit"
-                      class="bg-blue-500 hover:bg-blue-700 text-white text-sm font-semibold py-2 px-4 rounded"
-                    >
-                      Login
-                    </button>
-                    <br></br>
-                    <button
-                      //onClick={handleLogout}
-                      class="bg-blue-500 hover:bg-blue-700 text-white text-sm font-semibold py-2 px-4 rounded"
-                    >
-                      Logout
-                    </button>
-                    
-                  </div>
-                </form>
-                <br></br>
-                <br></br>
-                <div>
+                  <div>
                   <input
                       type="checkbox"
                       value="student"
@@ -178,16 +180,33 @@ export default function LoginForm() {
                       class="mr-2"
                   />Log in as a Teacher 
                 </div>
+                  
+                  <div className="no-underline hover:underline text-blue-dark text-xs">
+                    
+                    <button
+                      type="submit"
+                      className="bg-[#E4DCCF] hover:bg-blue-700 text-black text-sm  py-2 px-4 rounded float-center mr-4"                    >
+                      Login
+                    </button>
+                    <br></br>
+                    
+                    
+                    
+                  </div>
+                </form>
+                <br></br>
+                <br></br>
+                
               </div>
             </div>
           </div>
+          
           <div
-            class="hidden md:block md:w-1/2 rounded-r-lg
-				bg-[url(https://images.unsplash.com/photo-1515965885361-f1e0095517ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3300&q=80)] 
-
-			"
-            style={{ backgroundSize: "cover", backgroundPosition: "center" }}
-          ></div>
+             className="hidden md:block md:w-1/2 rounded-r-lg
+             bg-[url(https://o.remove.bg/downloads/2531ddcb-97af-4683-8ae5-dec96d9dfc68/undraw_Completing_re_i7ap-removebg-preview.png)]"
+          
+          
+          style={{ backgroundSize: "cover", backgroundPosition: "center" }}          ></div>
         </div>
       </div>
     </div>

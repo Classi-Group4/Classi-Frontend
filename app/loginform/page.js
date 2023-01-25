bcde56ff216af6bd08dc5d0184f3985d72414419
Shaden Shanab <shadenshanab2@gmail.com>
2023-01-25 13:57:16 +0000

    fix login form

--- app/loginform/page.js
+++ app/loginform/page.js
@@ -1,17 +1,11 @@
 'use client';
-
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import axios from "axios";
-
 import { useRouter } from "next/navigation";
-
 import cookieCutter from 'cookie-cutter';
-
-
 export default function LoginForm() {
-  
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   // const [checked,setChecked] = useState(false);
@@ -30,77 +24,81 @@ export default function LoginForm() {
     }
     // console.log(role)
   };
-
-  
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     try {
       const res = await fetch('http://127.0.0.1:8000/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         credentials: 'include',
         body: JSON.stringify({  email, password,is_teacher,is_student,role }),
-        
       });
-      
       const data = await res.json();
-      
       console.log(data.jwt)
-      
       if (data.jwt){
         cookieCutter.set('jwt', data.jwt)
         push('/');
-        
       } else {
         console.log("incorrect user details")
         console.log(res)
       }
-      
-      
     } catch (err) {
       console.error(err);
     }
   }
 
- 
   return (
-    <div class=" h-screen w-screen ">
-      <div class="flex flex-col items-center flex-1 h-full justify-center px-4 sm:px-0 ">
-        <div
-          class="flex rounded-lg shadow-lg w-full sm:w-3/4 lg:w-1/2 
-		bg-white sm:mx-0 bg-opacity-70  drop-shadow-lg  rounded-md shadow-md"
+    <div className="flex justify-center bg-[#576F72] background-size: full; py-20 ">
+      <div className="flex flex-col items-center flex-1 h-full justify-center px-4 sm:px-0 ">
+      <div
+          className="flex rounded-lg shadow-lg w-full sm:w-3/4 lg:w-1/2
+    bg-glass bg-white bg-opacity-70  drop-shadow-lg  rounded-md shadow-md p-10 w-1/3"
           style={{ height: "500px" }}
         >
-          <div class="flex flex-col w-full md:w-1/2 p-4 ">
-            <div class="flex flex-col flex-1 justify-center mb-8">
-              <h1 class="text-4xl text-center font-thin">Welcome Back</h1>
-              <div class="w-full mt-4">
+          <div className="flex flex-col w-full md:w-1/2 p-4 ">
+          <div className="flex flex-col flex-1 justify-center mb-8">
+          <h1 className="text-4xl text-center font-normal mb-10">Welcome Back</h1>
+          <div className="w-full mt-4 ">
                  <form
                   onSubmit={handleSubmit}
-                  class="form-horizontal w-3/4 mx-auto bg-opacity-70  drop-shadow-lg  rounded-md shadow-md"
+                  className="form-horizontal w-3/4 mx-auto bg-opacity-70  drop-shadow-lg  "                  
                   method="POST"
                   action="#"
                 >
-                  <div class="flex flex-col my-6">
+                    <div class="mb-6">
                     <input
                       type="email"
                       name="email"
-                      placeholder="  Email"
+                      class="form-control block w-full px-4 py-2
+                        text-x2 font-normal text-gray-700
+                        bg-white bg-clip-padding border
+                        border-solid border-gray-300
+                        rounded transition ease-in-out m-0
+                        focus:text-gray-700 focus:bg-white
+                        focus:border-blue-600 focus:outline-none"
+                        id="exampleFormControlInput2"
+                      placeholder="Email"
                       onChange={e => setEmail(e.target.value)}
                       required
                     />
                   </div>
-                  <div class="flex flex-col my-6">
+                   <div class="mb-6">
                     <input
                       type="password"
                       name="password"
-                      placeholder="  Password"
+                      class="form-control block w-full px-4 py-2
+                        text-x2 font-normal text-gray-700
+                        bg-white bg-clip-padding border
+                        border-solid border-gray-300
+                        rounded transition ease-in-out m-0
+                        focus:text-gray-700 focus:bg-white
+                        focus:border-blue-600 focus:outline-none"
+                      placeholder="Password"
                       onChange={e => setPassword(e.target.value)}
                       required
                     />
                   </div>
-                  <div className="mt-6 ml-1">
+                  <div>
                   <input
                       type="checkbox"
                       value="student"
@@ -109,32 +107,24 @@ export default function LoginForm() {
                       // checked={checked}
                       onChange={handleCheck}
                       class="mr-2"
-                  />Log in as a Teacher 
+                  />Log in as a Teacher
                 </div>
-                  <div class="flex flex-col mt-6">
+                  <div className="no-underline hover:underline text-blue-dark text-xs py-2">
                     <button
                       type="submit"
-                      class="bg-blue-500 hover:bg-blue-700 text-white text-sm font-semibold py-2 px-4 rounded"
-                    >
+                      className="bg-[#E4DCCF] hover:bg-[#7D9D9C] text-black text-sm  py-2 px-4 rounded float-center mr-4"                    >
                       Login
                     </button>
-                   
-                    
+                    <br></br>
                   </div>
                 </form>
-                <br></br>
-                <br></br>
-
               </div>
             </div>
           </div>
           <div
-            class="hidden md:block md:w-1/2 rounded-r-lg
-				bg-[url(https://images.unsplash.com/photo-1515965885361-f1e0095517ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3300&q=80)] 
-
-			"
-            style={{ backgroundSize: "cover", backgroundPosition: "center" }}
-          ></div>
+             className="hidden  md:block md:w-1/2 rounded-r-lg
+             bg-[url(https://cdn.discordapp.com/attachments/1062676267507384395/1067801791779045457/undraw_completing_re_i7ap-1289x1536-removebg-preview.png)]"
+          style={{ backgroundSize: "cover", backgroundPosition: "center" }}          ></div>
         </div>
       </div>
     </div>

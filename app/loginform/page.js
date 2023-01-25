395ba7e8c67de9d41fdcc3a5cf3f429852067e86
Shaden Shanab <shadenshanab2@gmail.com>
2023-01-25 14:03:29 +0000

    fix login form

--- app/loginform/page.js
+++ app/loginform/page.js
@@ -17,10 +17,10 @@ export default function LoginForm() {
     // setChecked(!checked);
     is_teacher = !is_teacher
     is_student = !is_student
-    if (is_teacher){
+    if (is_teacher) {
       role = "teacher"
-    } else{
-      role="student"
+    } else {
+      role = "student"
     }
     // console.log(role)
   };
@@ -31,11 +31,11 @@ export default function LoginForm() {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         credentials: 'include',
-        body: JSON.stringify({  email, password,is_teacher,is_student,role }),
+        body: JSON.stringify({ email, password, is_teacher, is_student, role }),
       });
       const data = await res.json();
       console.log(data.jwt)
-      if (data.jwt){
+      if (data.jwt) {
         cookieCutter.set('jwt', data.jwt)
         push('/');
       } else {
@@ -50,22 +50,22 @@ export default function LoginForm() {
   return (
     <div className="flex justify-center bg-[#576F72] background-size: full; py-20 ">
       <div className="flex flex-col items-center flex-1 h-full justify-center px-4 sm:px-0 ">
-      <div
+        <div
           className="flex rounded-lg shadow-lg w-full sm:w-3/4 lg:w-1/2
     bg-glass bg-white bg-opacity-70  drop-shadow-lg  rounded-md shadow-md p-10 w-1/3"
           style={{ height: "500px" }}
         >
           <div className="flex flex-col w-full md:w-1/2 p-4 ">
-          <div className="flex flex-col flex-1 justify-center mb-8">
-          <h1 className="text-4xl text-center font-normal mb-10">Welcome Back</h1>
-          <div className="w-full mt-4 ">
-                 <form
+            <div className="flex flex-col flex-1 justify-center mb-8">
+              <h1 className="text-4xl text-center font-normal mb-10">Welcome Back</h1>
+              <div className="w-full mt-4 ">
+                <form
                   onSubmit={handleSubmit}
-                  className="form-horizontal w-3/4 mx-auto bg-opacity-70  drop-shadow-lg  "                  
+                  className="form-horizontal w-3/4 mx-auto bg-opacity-70  drop-shadow-lg  "
                   method="POST"
                   action="#"
                 >
-                    <div class="mb-6">
+                  <div class="mb-6">
                     <input
                       type="email"
                       name="email"
@@ -76,13 +76,13 @@ export default function LoginForm() {
                         rounded transition ease-in-out m-0
                         focus:text-gray-700 focus:bg-white
                         focus:border-blue-600 focus:outline-none"
-                        id="exampleFormControlInput2"
+                      id="exampleFormControlInput2"
                       placeholder="Email"
                       onChange={e => setEmail(e.target.value)}
                       required
                     />
                   </div>
-                   <div class="mb-6">
+                  <div class="mb-6">
                     <input
                       type="password"
                       name="password"
@@ -99,7 +99,7 @@ export default function LoginForm() {
                     />
                   </div>
                   <div>
-                  <input
+                    <input
                       type="checkbox"
                       value="student"
                       name="remember"
@@ -107,24 +107,25 @@ export default function LoginForm() {
                       // checked={checked}
                       onChange={handleCheck}
                       class="mr-2"
-                  />Log in as a Teacher
-                </div>
+                    />Log in as a Teacher
+                  </div>
+
                   <div className="no-underline hover:underline text-blue-dark text-xs py-2">
+
                     <button
                       type="submit"
-                      className="bg-[#E4DCCF] hover:bg-[#7D9D9C] text-black text-sm  py-2 px-4 rounded float-center mr-4"                    >
+                      className="bg-[#E4DCCF] hover:bg-[#7D9D9C] text-black text-sm py-2 px-4 rounded float-center mr-4"                    >
                       Login
                     </button>
-                    <br></br>
                   </div>
                 </form>
               </div>
             </div>
           </div>
           <div
-             className="hidden  md:block md:w-1/2 rounded-r-lg
-             bg-[url(https://cdn.discordapp.com/attachments/1062676267507384395/1067801791779045457/undraw_completing_re_i7ap-1289x1536-removebg-preview.png)]"
-          style={{ backgroundSize: "cover", backgroundPosition: "center" }}          ></div>
+            className="hidden  md:block md:w-1/2 rounded-r-lg
+             bg-[url(https://media.discordapp.net/attachments/1062676267507384395/1067803236716773396/undraw_Completing_re_i7ap-removebg-preview.png)]"
+            style={{ backgroundSize: "cover", backgroundPosition: "center" }}          ></div>
         </div>
       </div>
     </div>
